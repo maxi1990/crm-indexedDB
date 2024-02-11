162f594b1fdc35e4e7710126213486cc2b214896
max <max@gmail.com>
2024-02-11 12:29:22 -0300

    se agregao la funcion de eliminar en app.js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 (function() {
     let DB;
+    const listadoClientes = document.querySelector('#listado-clientes');
 
     document.addEventListener('DOMContentLoaded', () => {
         crearDB();
@@ -7,8 +8,34 @@
         if(window.indexedDB.open('crm', 1)) {
             obtenerClientes();
         }
+
+        listadoClientes.addEventListener('click', eliminarRegistro)
         
     });
+
+    function eliminarRegistro(e) {
+        if(e.target.classList.contains('eliminar')){
+            const idEliminar = Number(e.target.dataset.cliente);
+
+            const confirmar = confirm('deseas eliminar este cliente?');
+            if(confirmar){
+                const transaction = DB.transaction(['crm'], 'readwrite');
+                const objectStore = transaction.objectStore('crm');
+
+                objectStore.delete(idEliminar);
+
+                transaction.oncomplete = function () {
+                    console.log('eliminado..');
+
+                    e.target.parentElement.parentElement.remove();
+                }
+
+                transaction.onerror = function () {
+                    console.log('hubo un error..');
+                }
+            }
+        }
+    }
     
     // Código de IndexedDB
     function crearDB() {
@@ -77,7 +104,6 @@
                  if(cursor) {
                     const { nombre, empresa, email, telefono, id } = cursor.value;
                     
-                    const listadoClientes = document.querySelector('#listado-clientes');
                     listadoClientes.innerHTML += `
 
                         <tr>
@@ -93,7 +119,7 @@
                             </td>
                             <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5">
                                 <a href="editar-cliente.html?id=${id}" class="text-teal-600 hover:text-teal-900 mr-5">Editar</a>
-                                <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900">Eliminar</a>
+                                <a href="#" data-cliente="${id}" class="text-red-600 hover:text-red-900 eliminar">Eliminar</a>
                             </td>
                         </tr>
                     `;
